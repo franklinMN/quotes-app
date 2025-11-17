d394159bfcec254f9fbe00a360c71671851b7561
franklinMN <frankmnwinter@gmail.com.>
2025-11-18 03:16:32 +0530

    remove quotes on #plain

--- plain.route.js
+++ plain.route.js
@@ -1,17 +1,20 @@
 let isPlain = false;
 export function router() {
   const frames = document.querySelector(".frames");
+  const title = document.querySelector(".title");
   const dateTime = document.querySelector(".datetime");
   const hash = window.location.hash;
 
   if (hash === "#/plain") {
     frames.classList.add("hide");
     dateTime.classList.add("hide");
+    title.style.setProperty("--quotes", '""');
     // isPlain = true;
   } else if (hash === "#/tempquote") {
   } else {
     frames.classList.remove("hide");
     dateTime.classList.remove("hide");
+    title.style.setProperty("--quotes", '"“"');
     // isPlain = false;
   }
 }
